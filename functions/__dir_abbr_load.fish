966cad4825dd6d60dc58a931783d3f3a5ea8524b
Roman Coedo <romancoedo@gmail.com>
2018-06-28 11:28:58 +0200

    Fix splitting on abbreviations that contains more than one equal sign

--- functions/__dir_abbr_load.fish
+++ functions/__dir_abbr_load.fish
@@ -2,7 +2,7 @@ function __dir_abbr_load --argument file
     if __dir_abbr_prompt $file
         set -g dir_abbr_restore $fish_user_abbreviations
         for line in (cat < $file)
-            set -l params (string split "=" $line);
+            set -l params (string split -m 1 "=" $line);
             abbr -a $params[1] $params[2]
         end
     end
